165a94e49c631246a907cc332404df738097d337
Srimani <srimani.ravikumar@gmail.com>
2025-07-16 15:43:46 +0530

    written a simple java program to illustrate generic class usage

---
+++ Java/DemoGenericClass.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class DemoGenericClass {
+    public static void main(String[] args) {
+        GenericClass<String> obj = new GenericClass<String>();
+        obj.add("Srimani");
+        obj.add("Ravikumar");
+        obj.add("Nirmal");
+        obj.add("Srinivas");
+
+        obj.print();
+        obj.removeLast();
+        obj.print();
+    }
+}
+
+class GenericClass<T> {
+    private List<T> list = new ArrayList<T>();
+
+    public void add(T item) {
+        list.add(item);
+    }
+
+    public Boolean removeLast() {
+        if(list.isEmpty()) {
+            return false;
+        }
+
+        list.remove(list.size() - 1);
+        return true;
+    }
+
+    public void print() {
+        System.out.print("[ ");
+        for(T element : list) {
+            System.out.print(element + ", ");
+        }
+        System.out.println("]");
+    }
+}
