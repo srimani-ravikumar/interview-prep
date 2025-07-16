fe95c6f15251edf00b6f3273f8dd765399d4a7ed
Srimani <srimani.ravikumar@gmail.com>
2025-07-16 16:48:43 +0530

    written a simple java program for generic methods

---
+++ Java/DemoGenericMethod.java
@@ -0,0 +1,17 @@
+public class DemoGenericMethod {
+    public static void main(String[] args) {
+        ExampleGenericMethod.genericPrint(58);
+        ExampleGenericMethod.genericPrint("Srimanikandan");
+    }
+}
+
+class ExampleGenericMethod {
+    /**
+     * generic methods allows different data type within a single method
+     * It is an alternative to method overloading
+    */ 
+    public static <T> void genericPrint(T element) {
+        System.out.println("Type : " + element.getClass().getName());
+        System.out.println("Value : " + element);
+    }
+}
