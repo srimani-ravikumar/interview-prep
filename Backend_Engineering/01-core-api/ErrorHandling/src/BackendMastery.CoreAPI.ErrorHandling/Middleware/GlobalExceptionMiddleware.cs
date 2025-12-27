230464ac25c754738849226b779a63f1757fb2d8
Srimani <srimani.ravikumar@eschergroup.com>
2025-12-27 13:25:29 +0530

    restructured the error response into standard way

--- Backend_Engineering/01-core-api/ErrorHandling/src/BackendMastery.CoreAPI.ErrorHandling/Middleware/GlobalExceptionMiddleware.cs
+++ Backend_Engineering/01-core-api/ErrorHandling/src/BackendMastery.CoreAPI.ErrorHandling/Middleware/GlobalExceptionMiddleware.cs
@@ -1,6 +1,6 @@
-﻿using System.Net;
+﻿using BackendMastery.CoreAPI.ErrorHandling.Exceptions;
+using System.Net;
 using System.Text.Json;
-using BackendMastery.CoreAPI.ErrorHandling.Exceptions;
 
 namespace BackendMastery.CoreAPI.ErrorHandling.Middleware;
 
@@ -8,13 +8,16 @@ namespace BackendMastery.CoreAPI.ErrorHandling.Middleware;
 /// Centralized exception handling middleware.
 /// </summary>
 /// <remarks>
+/// <para>
 /// Intuition:
 /// - Controllers should not handle cross-cutting concerns
 /// - One place to log, translate, and respond
-///
+///</para>
+///<para>
 /// Use case:
 /// - Consistent error responses
 /// - Centralized alerting
+/// </para>
 /// <para><b>Catch → Log → Handle → Retry / Fail</b></para>
 /// </remarks>
 public class GlobalExceptionMiddleware
@@ -52,11 +55,15 @@ public class GlobalExceptionMiddleware
 
         context.Response.StatusCode = (int)statusCode;
 
+        // Utlizing Anonymous type feature in C# to avoid loading unneccessary types onto the heap memory
+        // Structured response for the client to take next steps
         var problem = new
         {
-            title = statusCode.ToString(),
+            timeStamp = DateTime.Now,
             status = (int)statusCode,
-            detail = ex.Message
+            title = statusCode.ToString(),
+            message = ex.Message,
+            path = context.Request.Path
         };
 
         await context.Response.WriteAsync(JsonSerializer.Serialize(problem));
